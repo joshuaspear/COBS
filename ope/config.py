1a25ef6682ea068cfba6c8b8c2495811d4e4c0a0
Joshua Spear <josh.spear9@gmail.com>
2024-07-04 12:22:53 +0100

    updated config

--- ope/config.py
+++ ope/config.py
@@ -1,6 +1,5 @@
 import os
-d = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))
-    )
-EXPERIMENT_DIR = os.path.join(d,"paper_exp_output")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+DATA_LOC = os.path.join("/".join(dir_path.split('/')[0:-3]), "data")
+EXPERIMENT_DIR = os.path.join(DATA_LOC,"paper_exp_output")
 TRACKER_PATH = os.path.join(EXPERIMENT_DIR,"paper_tracker.json")
